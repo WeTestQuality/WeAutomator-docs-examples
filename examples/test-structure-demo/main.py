73041c7f229fc2615ab1fda32343dbbf8c5c1060
zhaojunye <zhaojunye@tencent.com>
2022-07-20 17:32:06 +0800

    main function file of test-structure-demo folder

---
+++ examples/test-structure-demo/main.py
@@ -0,0 +1,5 @@
+# -*- coding: UTF-8 -*-
+from uitrace.api import *
+
+pytest_main([os.path.join(os.path.dirname(__file__), "test_class_mode.py")])
+# pytest_main([os.path.join(os.path.dirname(__file__), "test_function_mode.py")])
